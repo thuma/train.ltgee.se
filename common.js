c529d17919c0d8d1fd254a15797c561ea29a1e75
Martin Thuresson <martin.thure@gmail.com>
2020-09-04 13:10:37 +0200

    Added html

---
+++ common.js
@@ -0,0 +1,61 @@
+var Success = {"install": function (Vue, options) {
+
+  Vue.prototype.copylink = function () {
+    var copyText = document.getElementById("correctlink");
+    copyText.select();
+    document.execCommand("copy");
+  };
+
+  Vue.prototype.successImage = function () {
+    var gifs = [
+    'https://media.giphy.com/media/nXxOjZrbnbRxS/giphy.gif',
+    'https://media.giphy.com/media/11sBLVxNs7v6WA/giphy.gif',
+    'https://media.giphy.com/media/geslvCFM31sFW/giphy.gif',
+    'https://media.giphy.com/media/vmon3eAOp1WfK/giphy.gif',
+    'https://media.giphy.com/media/WUq1cg9K7uzHa/giphy.gif',
+    'https://media.giphy.com/media/NbXTwsoD7hvag/giphy.gif',
+    'https://media.giphy.com/media/GCvktC0KFy9l6/giphy.gif',
+    'https://media.giphy.com/media/8xSnw21AM7OQo/giphy.gif',
+    'https://media.giphy.com/media/d2YV5Qqb5n1WOwCs/giphy.gif',
+    'https://media.giphy.com/media/1dagNhv8Oqu6l8U3ZK/source.gif',
+    'https://media.giphy.com/media/l0MYxef0mpdcnQnvi/giphy.gif',
+    'https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif',
+    'https://media.giphy.com/media/wIIEhq3uoiTdabD2pv/giphy.gif',
+    'https://media.giphy.com/media/EcUs5ri6QmzaU/giphy.gif',
+    'https://i.pinimg.com/originals/d6/7b/af/d67baffb519cab66bf603e565d9298f1.gif',
+    'https://media.giphy.com/media/DYH297XiCS2Ck/giphy.gif',
+    'https://media.giphy.com/media/31lPv5L3aIvTi/giphy.gif',
+    'https://media.giphy.com/media/kEKcOWl8RMLde/giphy.gif'
+    ];
+  
+    if(gifs[this.antal]){
+      return gifs[this.antal];
+    }
+    return gifs[Math.floor(Math.random()*gifs.length)];
+    }
+  }
+}
+
+var SaveAndRestore = {
+  "install": function (Vue, options) {
+    Vue.mixin({
+      methods: {
+        save: function () {
+          localStorage.setItem(this.taskname,JSON.stringify(this.$data))
+        }
+      },
+      created: function () {
+        if(window.localStorage[this.taskname]){
+          var myApp = this;
+          var stored = JSON.parse(window.localStorage[this.taskname]);
+          Object.keys(stored).forEach(function (field){
+            myApp[field] = stored[field];
+          });
+        }
+        else {
+         this.next();
+        }
+      }
+    })
+  }
+}
